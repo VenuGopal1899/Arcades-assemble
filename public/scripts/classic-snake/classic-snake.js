14bedb922990c1e612f987f3c28ccb8b1aa52030
VenuGopal1899 <venumalladi18@gmail.com>
2021-05-13 20:27:55 +0530

    JS Code added to change innerHTML

--- public/scripts/classic-snake/classic-snake.js
+++ public/scripts/classic-snake/classic-snake.js
@@ -541,7 +541,17 @@ function checkLoginStatus(){
   }
 }
 
+const scoresList = document.getElementsByClassName("members-with-score")[0];
+
 async function getScores(){
-  const res = await getLeaderboardScores(gameName);
-  // console.log(res);
+    const res = await getLeaderboardScores(gameName);
+    var length = (res.length >= 5) ? 5 : res.length;
+    var innerhtml = "";
+    for(var i=0; i<length; i++){
+        currScoreHtml = `<div class="single-member-score"><div class="position_number">${i+1}</div><div class="details-for-member"><div class="profile-pic"></div><span class="name">${res[i].ign}</span><span class="score">${res[i].score} pts.</span></div>`
+        innerhtml = innerhtml + currScoreHtml;
+    }
+    console.log('innerhtml ' ,innerhtml);
+    scoresList.innerHTML = innerhtml;
+    // console.log(res);
 }
