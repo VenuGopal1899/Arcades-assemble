8bfc77ee6f25e649f0d69b66db82ac8bbc39c279
VenuGopal1899 <venumalladi18@gmail.com>
2021-05-18 12:19:00 +0530

    Game improved and leaderboard api added

--- public/scripts/flappy-bird/game.js
+++ public/scripts/flappy-bird/game.js
@@ -57,14 +57,6 @@ scrn.addEventListener("click",()=>{
             bird.flap();
             break;
         case state.gameOver :
-            endTimeStamp = new Date();
-            const duration_mins = parseFloat((endTimeStamp.getTime() - startTimeStamp.getTime())/60000).toFixed(3);
-            if(isLoggedIn){
-                recordDurationStatistics(gameName, duration_mins);
-                var payloadObject = JSON.parse(atob(localStorage.getItem("JWT").split('.')[1]));
-                addScoreToLeaderboard(gameName, payloadObject.ign, payloadObject.hashedEmail, gameScore);
-            }
-
             state.curr = state.getReady;
             bird.speed = 0;
             bird.y = 100;
@@ -174,191 +166,200 @@ const pipe = {
 
 };
 const bird = {
-   animations :
-       [
-           {sprite : new Image()},
-           {sprite : new Image()},
-           {sprite : new Image()},
-           {sprite : new Image()},
-       ],
-   rotatation : 0,
-   x : 50,
-   y :100,
-   speed : 0,
-   gravity : .25,
-   thrust : 4.8,
-   frame:0,
-   draw : function() {
-       let h = this.animations[this.frame].sprite.height;
-       let w = this.animations[this.frame].sprite.width;
-       sctx.save();
-       sctx.translate(this.x,this.y);
-       sctx.rotate(this.rotatation*RAD);
-       sctx.drawImage(this.animations[this.frame].sprite,-w/2,-h/2);
-       sctx.restore();
-   },
-   update : function() {
-       let r = parseFloat( this.animations[0].sprite.width)/2;
-       switch (state.curr) {
-           case state.getReady :
-               this.rotatation = 0;
-               this.y +=(frames%10==0) ? Math.sin(frames*RAD) :0;
-               this.frame += (frames%10==0) ? 1 : 0;
-               break;
-           case state.Play :
-               this.frame += (frames%5==0) ? 1 : 0;
-               this.y += this.speed;
-               this.setRotation()
-               this.speed += this.gravity;
-               if(this.y + r  >= gnd.y||this.collisioned())
-               {
-                   state.curr = state.gameOver;
-               }
+    animations :
+        [
+            {sprite : new Image()},
+            {sprite : new Image()},
+            {sprite : new Image()},
+            {sprite : new Image()},
+        ],
+    rotatation : 0,
+    x : 50,
+    y :100,
+    speed : 0,
+    gravity : .25,
+    thrust : 4.8,
+    frame:0,
+    draw : function() {
+        let h = this.animations[this.frame].sprite.height;
+        let w = this.animations[this.frame].sprite.width;
+        sctx.save();
+        sctx.translate(this.x,this.y);
+        sctx.rotate(this.rotatation*RAD);
+        sctx.drawImage(this.animations[this.frame].sprite,-w/2,-h/2);
+        sctx.restore();
+    },
+    update : function() {
+        let r = parseFloat( this.animations[0].sprite.width)/2;
+            switch (state.curr) {
+                case state.getReady :
+                    this.rotatation = 0;
+                    this.y +=(frames%10==0) ? Math.sin(frames*RAD) :0;
+                    this.frame += (frames%10==0) ? 1 : 0;
+                    break;
+                case state.Play :
+                    this.frame += (frames%5==0) ? 1 : 0;
+                    this.y += this.speed;
+                    this.setRotation()
+                    this.speed += this.gravity;
+                    if(this.y + r  >= gnd.y || this.collisioned())
+                    {
+                        if(isLoggedIn){
+                            endTimeStamp = new Date();
+                            const duration_mins = parseFloat((endTimeStamp.getTime() - startTimeStamp.getTime())/60000).toFixed(3);
+                            if(isLoggedIn){
+                                recordDurationStatistics(gameName, duration_mins);
+                                var payloadObject = JSON.parse(atob(localStorage.getItem("JWT").split('.')[1]));
+                                addScoreToLeaderboard(gameName, payloadObject.ign, payloadObject.hashedEmail, gameScore);
+                            }
+                            state.curr = state.gameOver;
+                        } else {
+                            state.curr = state.gameOver;
+                        }
+                    }
 
-               break;
-           case state.gameOver :
-               this.frame = 1;
-               if(this.y + r  < gnd.y) {
-                   this.y += this.speed;
-                   this.setRotation()
-                   this.speed += this.gravity*2;
-               }
-               else {
-               this.speed = 0;
-               this.y=gnd.y-r;
-               this.rotatation=90;
-               if(!SFX.played) {
-                   SFX.die.play();
-                   SFX.played = true;
-               }
-               }
+                    break;
+                case state.gameOver :
+                    this.frame = 1;
+                    if(this.y + r  < gnd.y) {
+                        this.y += this.speed;
+                        this.setRotation()
+                        this.speed += this.gravity*2;
+                    }
+                    else {
+                    this.speed = 0;
+                    this.y=gnd.y-r;
+                    this.rotatation=90;
+                    if(!SFX.played) {
+                        SFX.die.play();
+                        SFX.played = true;
+                    }
+                    }
 
-               break;
-       }
-       this.frame = this.frame%this.animations.length;
-   },
-   flap : function(){
-       if(this.y > 0)
-       {
-           SFX.flap.play();
-           this.speed = -this.thrust;
-       }
-   },
-   setRotation : function(){
-       if(this.speed <= 0)
-       {
+                    break;
+            }
+        this.frame = this.frame%this.animations.length;
+    },
+    flap : function(){
+        if(this.y > 0)
+        {
+            SFX.flap.play();
+            this.speed = -this.thrust;
+        }
+    },
+    setRotation : function(){
+        if(this.speed <= 0)
+        {
 
-           this.rotatation = Math.max(-25, -25 * this.speed/(-1*this.thrust));
-       }
-       else if(this.speed > 0 ) {
-           this.rotatation = Math.min(90, 90 * this.speed/(this.thrust*2));
-       }
-   },
-   collisioned : function(){
-       if(!pipe.pipes.length) return;
-       let bird = this.animations[0].sprite;
-       let x = pipe.pipes[0].x;
-       let y = pipe.pipes[0].y;
-       let r = bird.height/4 +bird.width/4;
-       let roof = y + parseFloat(pipe.top.sprite.height);
-       let floor = roof + pipe.gap;
-       let w = parseFloat(pipe.top.sprite.width);
-       if(this.x + r>= x)
-       {
-           if(this.x + r < x + w)
-           {
-               if(this.y - r <= roof || this.y + r>= floor)
-               {
-                   SFX.hit.play();
-                   return true;
-               }
+            this.rotatation = Math.max(-25, -25 * this.speed/(-1*this.thrust));
+        }
+        else if(this.speed > 0 ) {
+            this.rotatation = Math.min(90, 90 * this.speed/(this.thrust*2));
+        }
+    },
+    collisioned : function(){
+        if(!pipe.pipes.length) return;
+        let bird = this.animations[0].sprite;
+        let x = pipe.pipes[0].x;
+        let y = pipe.pipes[0].y;
+        let r = bird.height/4 +bird.width/4;
+        let roof = y + parseFloat(pipe.top.sprite.height);
+        let floor = roof + pipe.gap;
+        let w = parseFloat(pipe.top.sprite.width);
+        if(this.x + r>= x)
+        {
+            if(this.x + r < x + w)
+            {
+                if(this.y - r <= roof || this.y + r>= floor)
+                {
+                    SFX.hit.play();
+                    return true;
+                }
 
-           }
-           else if(pipe.moved)
-           {
-               UI.score.curr++;
-               SFX.score.play();
-               pipe.moved = false;
-           }
+            }
+            else if(pipe.moved)
+            {
+                UI.score.curr++;
+                gameScore = UI.score.curr;
+                SFX.score.play();
+                pipe.moved = false;
+            }
+        }
+    }
+    };
+    const UI = {
+    getReady : {sprite : new Image()},
+    gameOver : {sprite : new Image()},
+    tap : [{sprite : new Image()},
+            {sprite : new Image()}],
+    score : {
+        curr : 0,
+        best : 0,
+    },
+    x :0,
+    y :0,
+    tx :0,
+    ty :0,
+    frame : 0,
+    draw : function() {
+        switch (state.curr) {
+            case state.getReady :
+                this.y = parseFloat(scrn.height-this.getReady.sprite.height)/2;
+                this.x = parseFloat(scrn.width-this.getReady.sprite.width)/2;
+                this.tx = parseFloat(scrn.width - this.tap[0].sprite.width)/2;
+                this.ty = this.y + this.getReady.sprite.height- this.tap[0].sprite.height;
+                sctx.drawImage(this.getReady.sprite,this.x,this.y);
+                sctx.drawImage(this.tap[this.frame].sprite,this.tx,this.ty)
+                break;
+            case state.gameOver :
+                this.y = parseFloat(scrn.height-this.gameOver.sprite.height)/2;
+                this.x = parseFloat(scrn.width-this.gameOver.sprite.width)/2;
+                this.tx = parseFloat(scrn.width - this.tap[0].sprite.width)/2;
+                this.ty = this.y + this.gameOver.sprite.height- this.tap[0].sprite.height;
+                sctx.drawImage(this.gameOver.sprite,this.x,this.y);
+                sctx.drawImage(this.tap[this.frame].sprite,this.tx,this.ty)
+                break;
+        }
+        this.drawScore();
+    },
+    drawScore : function() {
+            sctx.fillStyle = "#FFFFFF";
+            sctx.strokeStyle = "#000000";
+        switch (state.curr) {
+            case state.Play :
+                sctx.lineWidth = "2";
+                sctx.font = "35px Squada One";
+                sctx.fillText(this.score.curr,scrn.width/2-5,50);
+                sctx.strokeText(this.score.curr,scrn.width/2-5,50);
+                break;
+            case state.gameOver :
+                    sctx.lineWidth = "2";
+                    sctx.font = "40px Squada One";
+                    let sc = `SCORE :     ${this.score.curr}`;
+                    try {
+                        this.score.best = Math.max(this.score.curr,localStorage.getItem("best"));
+                        localStorage.setItem("best",this.score.best);
+                        let bs = `BEST  :     ${this.score.best}`;
+                        sctx.fillText(sc,scrn.width/2-80,scrn.height/2+0);
+                        sctx.strokeText(sc,scrn.width/2-80,scrn.height/2+0);
+                        sctx.fillText(bs,scrn.width/2-80,scrn.height/2+30);
+                        sctx.strokeText(bs,scrn.width/2-80,scrn.height/2+30);
+                    }
+                    catch(e) {
+                        sctx.fillText(sc,scrn.width/2-85,scrn.height/2+15);
+                        sctx.strokeText(sc,scrn.width/2-85,scrn.height/2+15);
+                    }
 
+                break;
+        }
+    },
+    update : function() {
+        if(state.curr == state.Play) return;
+        this.frame += (frames % 10==0) ? 1 :0;
+        this.frame = this.frame % this.tap.length;
+    }
 
-
-       }
-   }
-};
-const UI = {
-   getReady : {sprite : new Image()},
-   gameOver : {sprite : new Image()},
-   tap : [{sprite : new Image()},
-          {sprite : new Image()}],
-   score : {
-       curr : 0,
-       best : 0,
-   },
-   x :0,
-   y :0,
-   tx :0,
-   ty :0,
-   frame : 0,
-   draw : function() {
-       switch (state.curr) {
-           case state.getReady :
-               this.y = parseFloat(scrn.height-this.getReady.sprite.height)/2;
-               this.x = parseFloat(scrn.width-this.getReady.sprite.width)/2;
-               this.tx = parseFloat(scrn.width - this.tap[0].sprite.width)/2;
-               this.ty = this.y + this.getReady.sprite.height- this.tap[0].sprite.height;
-               sctx.drawImage(this.getReady.sprite,this.x,this.y);
-               sctx.drawImage(this.tap[this.frame].sprite,this.tx,this.ty)
-               break;
-           case state.gameOver :
-               this.y = parseFloat(scrn.height-this.gameOver.sprite.height)/2;
-               this.x = parseFloat(scrn.width-this.gameOver.sprite.width)/2;
-               this.tx = parseFloat(scrn.width - this.tap[0].sprite.width)/2;
-               this.ty = this.y + this.gameOver.sprite.height- this.tap[0].sprite.height;
-               sctx.drawImage(this.gameOver.sprite,this.x,this.y);
-               sctx.drawImage(this.tap[this.frame].sprite,this.tx,this.ty)
-               break;
-       }
-       this.drawScore();
-   },
-   drawScore : function() {
-           sctx.fillStyle = "#FFFFFF";
-           sctx.strokeStyle = "#000000";
-       switch (state.curr) {
-           case state.Play :
-               sctx.lineWidth = "2";
-               sctx.font = "35px Squada One";
-               sctx.fillText(this.score.curr,scrn.width/2-5,50);
-               sctx.strokeText(this.score.curr,scrn.width/2-5,50);
-               break;
-           case state.gameOver :
-                   sctx.lineWidth = "2";
-                   sctx.font = "40px Squada One";
-                   let sc = `SCORE :     ${this.score.curr}`;
-                   try {
-                       this.score.best = Math.max(this.score.curr,localStorage.getItem("best"));
-                       localStorage.setItem("best",this.score.best);
-                       let bs = `BEST  :     ${this.score.best}`;
-                       sctx.fillText(sc,scrn.width/2-80,scrn.height/2+0);
-                       sctx.strokeText(sc,scrn.width/2-80,scrn.height/2+0);
-                       sctx.fillText(bs,scrn.width/2-80,scrn.height/2+30);
-                       sctx.strokeText(bs,scrn.width/2-80,scrn.height/2+30);
-                   }
-                   catch(e) {
-                       sctx.fillText(sc,scrn.width/2-85,scrn.height/2+15);
-                       sctx.strokeText(sc,scrn.width/2-85,scrn.height/2+15);
-                   }
-
-               break;
-       }
-   },
-   update : function() {
-       if(state.curr == state.Play) return;
-       this.frame += (frames % 10==0) ? 1 :0;
-       this.frame = this.frame % this.tap.length;
-   }
-
-};
+    };
 
 gnd.sprite.src="../../resources/flappy-bird/img/ground.png";
 bg.sprite.src="../../resources/flappy-bird/img/BG.png";
